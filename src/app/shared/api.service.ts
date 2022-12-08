4f602699fdae504c4876a1b5703d6d301c8d5209
Arun Augustine <arunaugustine003@gmail.com>
2022-12-08 18:00:48 +0530

    Changed Service from Heroku to Glitch

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -9,28 +9,28 @@ export class ApiService {
   constructor(private http: HttpClient) {}
 
   postEmployee(data: any) {
-    return this.http.post<any>('https://angular-customer-manager.herokuapp.com/customers/', data).pipe(
+    return this.http.post<any>('https://customer-manager-db-by-arun.glitch.me/customers/', data).pipe(
       map((res: any) => {
         return res;
       })
     );
   }
   getEmployee() {
-    return this.http.get<any>('https://angular-customer-manager.herokuapp.com/customers/').pipe(
+    return this.http.get<any>('https://customer-manager-db-by-arun.glitch.me/customers/').pipe(
       map((res: any) => {
         return res;
       })
     );
   }
   updateEmployee(data: any, id: number) {
-    return this.http.put<any>('https://angular-customer-manager.herokuapp.com/customers/' + id, data).pipe(
+    return this.http.put<any>('https://customer-manager-db-by-arun.glitch.me/customers/' + id, data).pipe(
       map((res: any) => {
         return res;
       })
     );
   }
   deleteEmployee(id: number) {
-    return this.http.delete<any>('https://angular-customer-manager.herokuapp.com/customers/' + id).pipe(
+    return this.http.delete<any>('https://customer-manager-db-by-arun.glitch.me/customers/' + id).pipe(
       map((res: any) => {
         return res;
       })
